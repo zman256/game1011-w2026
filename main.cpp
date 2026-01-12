800b051058e3be685dcc0d7fdfbdebd48cb6e1a7
Smiley98 <smileysofficialcinema@gmail.com>
2026-01-12 11:18:19 -0500

    Added operators

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 
 int main()
 {
-	int health = 100;
-	float mana = 50.75f;
-	double speed = 25.3245234598023485902;
+	int health = 100 + 50;
+	float mana = 50.75f - 25.25f;
+	double speed = 25.3245234598023485902 * 2.222222222 / 1.111111111;
 	unsigned long long defense = 18446744073709551615;
 	std::cout << "Player has " << health << " health." << std::endl;
 	std::cout << "Player has " << mana << " mana." << std::endl;
 	std::cout << "Player has " << speed << " speed." << std::endl;
 	std::cout << "Defense before overflow: " << defense << std::endl;
+
+	int numerator = 10;
 	defense++;
+	//numerator /= defense; // Cannot divide by zero!!!
 	std::cout << "Defense after overflow: " << defense << std::endl;
+
 	const char* message = "Hello world!";	// String literal --> array of characters
 	std::string message2 = "Hello";
 	message2 += " world!";					// C++ strings (std::string) allow you to combine text
